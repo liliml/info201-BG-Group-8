72bd86513dd5c3b1c016a3afead109320d489455
Kelly Le <kle806@uw.edu>
2023-03-01 19:50:16 -0800

    Added panels back to separate text. Mostly finished, only needs touch ups on the look/design of text.

--- app/app.R
+++ app/app.R
@@ -1,5 +1,6 @@
 # Initial app prototype for Kelly's graphs.
 
+# Loading libraries
 library(shiny)
 library(tidyverse)
 
@@ -23,40 +24,45 @@ ui <- fluidPage(
     ),
     
     # Show a plot of the generated distribution
-    mainPanel(plotOutput("distPlot"),
-  
-        # Histogram description
-        p("Histogram Description:"),
-        p("These histograms contain data of each streaming service's movie catalog in 2021 to early 2022. 
+    mainPanel(
+      tabsetPanel(
+        
+        # Panel for histogram
+        tabPanel("Histogram", plotOutput("distPlot"),
+                 p("Histogram Description:"),
+                 p("These histograms contain data of each streaming service's movie catalog in 2021 to early 2022. 
+        The histogram's bins on the x axis represent the Rotten Tomatoes rating given to each movie. Ratings are points from 0 to 100.
         The histogram's bins on the x axis represent the ",
-        a("Rotten Tomatoes rating", href= 'https://www.rottentomatoes.com/about#:~:text=When%20at%20least%2060%25%20of,to%20indicate%20its%20Fresh%20status.&text=When%20less%20than%2060%25%20of,to%20indicate%20its%20Rotten%20status.'),
-        "given to each movie. Ratings are points from 0 to 100.
+                   a("Rotten Tomatoes rating", href= 'https://www.rottentomatoes.com/about#:~:text=When%20at%20least%2060%25%20of,to%20indicate%20its%20Fresh%20status.&text=When%20less%20than%2060%25%20of,to%20indicate%20its%20Rotten%20status.'),
+                   "given to each movie. Ratings are points from 0 to 100.
         The height of each bin corresponds to how many movies have gotten that rating.
-        The color scale on the bins are another way to depict the ratings. Green corresponds to higher ratings while red are lower ratings."),
+        The color scale on the bins are another way to depict the ratings. Green corresponds to higher ratings while red are lower ratings.")
+        ),
         
-        # Table
-        tableOutput("table"),
+        # Panel for table of total movies for each streaming service
+        tabPanel("Table", tableOutput("table"),
+                 p("Table Description:"),
+                 p("This table shows how many movies were available on each streaming service in 2021 to early 2022.")
+                 ),
         
-        # Table Description
-        p("Table Description:"),
-        p("This table shows how many movies were available on each streaming service in 2021 to early 2022."),
-        
-        # Analysis
-        p("Analysis:"),
+        # Panel for analysis
+        tabPanel("Analysis", p("Analysis:"),
         p("All streaming services have a variety of ratings and mostly follow a normal distribution shape.
         All histograms have some outlier data points where there are a handful of very low rated movies in their catalogs.
-        This is one area where start up companies making a streaming service that is competitive with the current market could improve on."),
-        p("Netflix seems to have the widest range of ratings in their movie catalog, while Hulu seem to have the most narrow.
-        An explanation for this could be that the size of Netflix's catalog is much larger than Hulu, almost having 1500 more movies.
-        Hulu appears to have the median with the highest rating as it's center is around the 60/100 Rotten Tomatoes rating. Disney+ also has a higher median than Netflix and Prime Video.
-        The service with the lowest median rating is Prime Video, with it's histogram looking to be skewed to the right. 
-        Prime Video also seems to have the lowest percentage of movies in its catalog that are highly rated as it's bins are the least green shaded and lean more towards red/yellow."),
-        p("There is a trend with larger movie catalogs and greater diversity of ratings, but moreso leaning towards the lower ratings spectrum while the smaller catalogs lean more towards the higher spectrum even with a more narrow ratings range.
-        It is possible that Hulu and Disney+ are more selective with what movies are on their catalog, hence why there are less but are of higher Rotten Tomatoes ratings. 
-        For start up companies, it's important to recognize the balance between having a large and diverse catalog of movies and a smaller scope that are of higher quality.")
+          This is one area where start up companies making a streaming service that is competitive with the current market could improve on."),
+          p("Netflix seems to have the widest range of ratings in their movie catalog, while Hulu seem to have the most narrow.
+          An explanation for this could be that the size of Netflix's catalog is much larger than Hulu, almost having 1500 more movies.
+          Hulu appears to have the median with the highest rating as it's center is around the 60/100 Rotten Tomatoes rating. Disney+ also has a higher median than Netflix and Prime Video.
+          The service with the lowest median rating is Prime Video, with it's histogram looking to be skewed to the right. 
+            Prime Video also seems to have the lowest percentage of movies in its catalog that are highly rated as it's bins are the least green shaded and lean more towards red/yellow."),
+            p("There is a trend with larger movie catalogs and greater diversity of ratings, but moreso leaning towards the lower ratings spectrum while the smaller catalogs lean more towards the higher spectrum even with a more narrow ratings range.
+            It is possible that Hulu and Disney+ are more selective with what movies are on their catalog, hence why there are less but are of higher Rotten Tomatoes ratings. 
+              For start up companies, it's important to recognize the balance between having a large and diverse catalog of movies and a smaller scope that are of higher quality.")
+        )
       )
     )
   )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
@@ -66,6 +72,7 @@ server <- function(input, output, session) {
   streaming$modified_ratings <- as.numeric(
       str_remove(streaming$`Rotten Tomatoes`, "/100"))
   
+  # Creating histogram plot
   output$distPlot <- renderPlot({
     
     # generate streaming service name based on input$service from ui.R
@@ -84,10 +91,16 @@ server <- function(input, output, session) {
            fill = "Ratings")
   })
   
-  #Creating table
+  # Creating table
   output$table <- renderTable({
-    total_Movies <- c(sum(streaming[,7] == 1), sum(streaming[,8] == 1), sum(streaming[,9] == 1), sum(streaming[,10] == 1))
     
+    # Vector with total number of movies on each streaming service
+    total_Movies <- c(sum(streaming[,7] == 1), 
+                      sum(streaming[,8] == 1), 
+                      sum(streaming[,9] == 1), 
+                      sum(streaming[,10] == 1))
+    
+    # Creating data table from names of services and total number of movies
     streaming_total_movies <- data.frame(streaming_services,total_Movies)
     streaming_total_movies
   })
